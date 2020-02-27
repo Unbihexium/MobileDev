ce3605fb071c3ca85f2f211cdf46e84e6b280997
Egor Nalobin <ednalobin@infodev.ru>
2020-02-27 10:18:20 +1000

    initial commit

---
+++ MobileDev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileDev
+//
+//  Created by Egor Nalobin on 27.02.2020.
+//  Copyright © 2020 Egor Nalobin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
